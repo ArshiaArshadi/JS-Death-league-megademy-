0bbbb2b98fd78578e9d2a1ce5811c842c4359f1c
ArshiaArshadi <arshia.arshadi1384@gmail.com>
2024-11-02 11:26:00 +0330

    feat : Primary data
    value/function/if & else

--- 11-even-and-odd/script.js
+++ 11-even-and-odd/script.js
@@ -1,3 +1,34 @@
 // Identifying odd and even numbers *11*
 
 // ------------------------------
+
+// create variable 
+let numberValue 
+
+// Using a loop to get an integer value from the user
+while (true){
+    // get user input with prompt
+    let getInputValue = prompt("Enter the desired number")
+
+
+    // Checking the numeric value of the input with the condition
+    if ( getInputValue === null || getInputValue !== Number ) {
+        continue
+    } else{
+        numberValue = getInputValue
+        break
+    }
+
+}
+
+
+// Checking whether it is even or odd
+if (numberValue % 2 !== 0 ){
+
+    document.body.innerHTML = `Your number is odd`;
+    
+}else {
+
+    document.body.innerHTML = `Your number is even`;
+
+}
