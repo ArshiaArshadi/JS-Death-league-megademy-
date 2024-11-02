5cb56ac877c7960a5a409041b29d739a6a9d636c
ArshiaArshadi <arshia.arshadi1384@gmail.com>
2024-11-02 11:38:27 +0330

    fix : bug (check odd or even)
    update 1

--- 11-even-and-odd/script.js
+++ 11-even-and-odd/script.js
@@ -6,27 +6,28 @@
 let numberValue 
 
 // Using a loop to get an integer value from the user
-while (true){
-    // get user input with prompt
-    let getInputValue = prompt("Enter the desired number")
+// while (true){
+//     // get user input with prompt
+     let getInputValue = Number(prompt("Enter the desired number"))
 
 
-    // Checking the numeric value of the input with the condition
-    if ( getInputValue === null || getInputValue !== Number ) {
-        continue
-    } else{
-        numberValue = getInputValue
-        break
-    }
+//     // Checking the numeric value of the input with the condition
+//     if ( getInputValue === null || !Number.isInteger(Number(getInputValue)) ) {
+//         continue
+//     } else{
+//         numberValue = getInputValue
+//         break
+//     }
 
-}
+// }
 
 
 // Checking whether it is even or odd
-if (numberValue % 2 !== 0 ){
+if (getInputValue % 2 !== 0 ){
 
     // Check for odd
-    document.body.innerHTML = `Your number is odd`;
+    // document.body.innerHTML 
+    alert('Your number is odd');
     
 }else {
 
