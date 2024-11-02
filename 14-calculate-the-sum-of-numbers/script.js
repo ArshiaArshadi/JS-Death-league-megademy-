25cd5ed3d63fd378bd142398b65f73d6821e38a7
ArshiaArshadi <arshia.arshadi1384@gmail.com>
2024-11-02 12:46:20 +0330

    feat : create main element
    Dom element & js data

--- 14-calculate-the-sum-of-numbers/script.js
+++ 14-calculate-the-sum-of-numbers/script.js
@@ -1,20 +1,29 @@
 
 
 
+// select element
+let input = document.getElementById("input");
+let submit = document.getElementById("submit");
+let output = document.getElementById("output");
 
-
-let input = document.getElementById("#input");
-let output = document.getElementById("#output");
-let submit = document.querySelector("#submit");
-let numbers = [];
+// create varable
+let arrayNumber = [];
 let sum = 0;
-submit.addEventListener("click", () => {
-  let inputVal = input.value;
-  for (let i = 0; i <= inputVal; i++) {
-    numbers.push(i);
-  }
-  numbers.forEach((element) => {
-    sum = sum + element;
-  });
-  output.append(`the answer is ${sum}`);
-});
+
+// add event for get user input
+submit.addEventListener("click", sumNumber)
+
+
+// create function to add the numbers as stated in the challenge
+function sumNumber (){
+    let inputNumber = input.value;
+    
+    // 
+    for (let i = 0; i <= inputNumber; i++) {
+      numbers.push(i);
+    }
+    numbers.forEach((element) => {
+      sum = sum + element;
+    });
+    output.append(`the answer is ${sum}`);
+}
