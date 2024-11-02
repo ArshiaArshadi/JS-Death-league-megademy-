90d1389d29ccef8696bfed5b3652b73b38b9983f
ArshiaArshadi <arshia.arshadi1384@gmail.com>
2024-11-02 14:54:13 +0330

    fix : add comment and fix bug
    final change
    end project

---
+++ 07-dynamic-addition-subtraction/script.js
@@ -0,0 +1,34 @@
+// select element
+const courantNumber = document.querySelector("#number");
+const increaseBtn = document.querySelector("#increase");
+const resetBtn = document.querySelector("#reset");
+const decreaseBtn = document.querySelector("#decrease");
+
+// create varable
+let newNum;
+
+
+
+// event and function to increase the number
+increaseBtn.addEventListener("click", function () {
+  newNum = Number(courantNumber.innerHTML) + 1;
+  courantNumber.innerHTML = newNum;
+  // color for increase
+  courantNumber.style.color = "green";
+});
+
+
+// event and function to decrease the number
+decreaseBtn.addEventListener("click", function () {
+  newNum = Number(courantNumber.innerHTML) - 1;
+  courantNumber.innerHTML = newNum;
+  // color for decrease
+  courantNumber.style.color = "red";
+});
+
+
+// event and function to reset the number
+resetBtn.addEventListener("click", function () {
+  courantNumber.innerHTML = 0;
+  courantNumber.style.color = "yellow";
+});
