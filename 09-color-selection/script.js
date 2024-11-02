a7f2cc4c7e0f3f105c750ac74c76a1da6d0f317e
ArshiaArshadi <arshia.arshadi1384@gmail.com>
2024-11-02 14:32:11 +0330

    fix : final update to challenge 19
    add comment
    and end project

---
+++ 09-color-selection/script.js
@@ -0,0 +1,25 @@
+
+// select element
+let colorBTN = document.querySelector("#color-BTN");
+// add color list
+let colors = ["red", "blue", "green", "black"];
+
+// forech loop to create color element
+colors.forEach((element) => {
+  // create element for color
+  let createColor = document.createElement("div");
+  createColor.id = element;
+  createColor.style.backgroundColor = element;
+  
+  // add event to get user target
+  createColor.addEventListener("click", changeBackColor);
+
+  // add color element in DOM
+  colorBTN.append(createColor);
+});
+
+// function for change backgroud color with id
+function changeBackColor(e) {
+    let targetID = e.target.id;
+  document.body.style.backgroundColor = targetID;
+}
