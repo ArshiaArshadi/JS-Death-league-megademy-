26e966fc5fca879991b0596b5a725fb6cb25f275
ArshiaArshadi <arshia.arshadi1384@gmail.com>
2024-11-02 11:57:31 +0330

    feat : create element
    2 element for show text & clicks

---
+++ 13-click-count/script.js
@@ -0,0 +1,28 @@
+// Count the number of clicks *13*
+
+// ------------------------------
+
+// select button
+const button =  document.getElementById("btn")
+
+
+// add event fot check user click
+button.addEventListener("click", countClick)
+
+
+// create element for show text in DOM
+const textElement = document.createElement("H1")
+textElement.id = "textElement"
+document.body.appendChild("textElement")
+
+// create element for show clicks in DOM
+const clickElement = document.createElement("span")
+clickElement.id = "clickElement"
+document.body.appendChild("clickElement")
+
+
+function
+
+
+
+
